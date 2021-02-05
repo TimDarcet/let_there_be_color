7b51ce0d81071040d90db57501db875c4b44f982
Timothée Darcet <timothee.darcet+git@m4x.org>
2021-02-05 17:40:58 +0100

    source conda

--- launch_worker.sh
+++ launch_worker.sh
@@ -5,6 +5,6 @@ export MASTER_ADDR=$2
 export WORLD_SIZE=$3
 export NODE_RANK=$4
 
-conda init
+source ~/miniconda3/etc/profile.d/conda.sh
 conda activate pytorch
 python main.py --n_nodes $WORLD_SIZE
