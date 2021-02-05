ed1687d637331841bb4402465bd1152fcd89b323
Timothée Darcet <timothee.darcet+git@m4x.org>
2021-02-05 18:23:21 +0100

    local rank 0

--- launch_worker.sh
+++ launch_worker.sh
@@ -4,7 +4,7 @@ export MASTER_PORT=$1
 export MASTER_ADDR=$2
 export WORLD_SIZE=$3
 export NODE_RANK=$4
-export LOCAL_RANK=$NODE_RANK
+export LOCAL_RANK=0
 
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate pytorch
