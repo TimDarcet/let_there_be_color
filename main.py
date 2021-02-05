fc4c1059e512b1808e79d894e04d6f3c9dc4842b
Timothée Darcet <timothee.darcet+git@m4x.org>
2021-02-05 18:10:06 +0100

    List gpus to take

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def main(args):
     
     # train on 80 GPUs across 10 nodes
     trainer = Trainer(max_epochs=1,
-                      gpus=1,
+                      gpus=[0],
                       num_nodes=args.n_nodes,
                       accelerator='ddp')
     trainer.fit(model, MNISTDataModule(os.getcwd()))
